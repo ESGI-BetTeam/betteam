4d642006dc860e23d108a5adf2ea8426e34d46ed
Yohann <yoyo24du26@gmail.com>
2026-04-02 14:21:29 +0200

    feat(typography): adding a file to centralize all typo styles

--- apps/mobile/src/screens/components/ComponentScreen.tsx
+++ apps/mobile/src/screens/components/ComponentScreen.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { colors, spacing, fontSize } from '../../theme';
+import { colors, spacing, typo } from '@/theme';
 import { State } from '@/components/ui/State';
 
 export function ComponentScreen() {
   return (
     <SafeAreaView style={styles.safe}>
       <View style={styles.container}>
-        <Text style={styles.title}>Components</Text>
-        <Text style={styles.subTitle}>States :</Text>
-        <State variant="active" label='En Cours'/>
-        <State variant="live" isAnimated/>
-        <State variant="soon"/>
-        <State variant="finished"/>
+        <Text style={typo.h1}>Components</Text>
+
+        <Text style={typo.h2}>Typo :</Text>
+        <View style={styles.componentsContainer}>
+          <Text style={typo.h1}>Heading 1</Text>
+          <Text style={typo.h2}>Heading 2</Text>
+          <Text style={typo.h3}>Heading 3</Text>
+          <Text style={typo.h4}>Heading 4</Text>
+          <Text style={typo.p}>Paragraph</Text>
+          <Text style={typo.pBold}>Paragraph bold</Text>
+          <Text style={typo.pMuted}>Paragraph muted</Text>
+          <Text style={typo.small}>Small paragraph</Text>
+        </View>
+
+        <Text style={typo.h2}>States :</Text>
+        <View style={styles.componentsContainer}>
+          <State variant="active" label='En Cours'/>
+          <State variant="live" isAnimated/>
+          <State variant="soon"/>
+          <State variant="finished"/>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -22,6 +36,5 @@ export function ComponentScreen() {
 const styles = StyleSheet.create({
   safe: { flex: 1, backgroundColor: colors.background},
   container: { flex: 1, padding: spacing.lg },
-  title: { fontSize: fontSize.xxl, fontWeight: '700', color: colors.textPrimary },
-  subTitle: { fontSize: fontSize.xl, fontWeight: '700', color: colors.textSecondary },
+  componentsContainer: { marginLeft: 40, marginRight: 40, marginBottom : 30 },
 });
--- apps/mobile/src/theme/index.ts
+++ apps/mobile/src/theme/index.ts
@@ -1,2 +1,3 @@
 export { colors } from './colors';
 export { spacing, radius, fontSize, borderWidth } from './spacing';
+export { typo } from './typo';
