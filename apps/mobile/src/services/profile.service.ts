fa0b975f8d75d15f5b33bd5c9c8d32841121ece9
Yohann <yoyo24du26@gmail.com>
2026-05-14 17:24:28 +0200

    feat(profile-page): creating the profile page and fetching user data

---
+++ apps/mobile/src/services/profile.service.ts
@@ -0,0 +1,19 @@
+import { api } from "./api";
+
+import { UserStats, UserWithStats } from "@/types/stats";
+import { User } from "./auth.service";
+
+export const profileService = {
+	async getProfile(): Promise<UserWithStats> {
+		const userResponse = await api.get('/auth/me');
+		const user = userResponse.data.user;
+
+		const statsResponse = await api.get<{ data: UserStats }>(`/stats/user/${user.id}`);
+		const stats = statsResponse.data.data;
+
+		return {
+			user,
+			stats,
+		}
+	}
+}
